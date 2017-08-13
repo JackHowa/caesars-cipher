ade798dae51e91a05d0d502dae5ef34b8a709d3e
jackhowa <johncarverhoward@gmail.com>
2017-08-13 16:13:54 -0500

    algorithm working for spaces; had to use modulo to shift 13

--- caesar.js
+++ caesar.js
@@ -1,21 +1,16 @@
-
-function rot13(str) { // LBH QVQ VG!
-  // find all the letters
-  // split the letters by the spaces 
-  // iterate through each letter group 
-    // iterate through each letter in each letter group 
-      // convert each letter by 13 forward in their unicode 
-    // add all of the letters back together 
-  // rearrange all of the converted letter groups back together 
-  // unsplit the letter groups 
-  // add the appropriate non-letters in the correct places 
-  // return the converted string 
-
-
-
-
-  return str;
+function rot13(str) { 
+  outputStr = ""; 
+  for (i = 0; i < str.length; i++) {
+    if (str[i] == " ") {
+      outputStr += str[i];
+    }
+    else {
+      var letterASCII = str.charCodeAt(i);
+      var newLetterASCII = letterASCII % 26 + 65; 
+      var newLetter = String.fromCharCode(newLetterASCII);
+      outputStr += newLetter;
+    }
+    
+  }
+  return outputStr;
 }
-
-// Change the inputs below to test
-rot13("SERR PBQR PNZC");
