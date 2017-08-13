e5979fbe6e02934147d29a371eb6f827bccb2678
jackhowa <johncarverhoward@gmail.com>
2017-08-13 15:53:57 -0500

    add pseudocode with some unkown unkowns

--- caesar.js
+++ caesar.js
@@ -1,6 +1,19 @@
 
 function rot13(str) { // LBH QVQ VG!
-  
+  // find all the letters
+  // split the letters by the spaces 
+  // iterate through each letter group 
+    // iterate through each letter in each letter group 
+      // convert each letter by 13 forward in their unicode 
+    // add all of the letters back together 
+  // rearrange all of the converted letter groups back together 
+  // unsplit the letter groups 
+  // add the appropriate non-letters in the correct places 
+  // return the converted string 
+
+
+
+
   return str;
 }
 
