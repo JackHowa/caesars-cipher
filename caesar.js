b6e0f252f8bb9ec282629e6751fdd9ae01f46346
jackhowa <johncarverhoward@gmail.com>
2017-08-13 16:20:33 -0500

    use the regex matching to match non letters

--- caesar.js
+++ caesar.js
@@ -1,16 +1,20 @@
 function rot13(str) { 
   outputStr = ""; 
+  var regex = /[A-Z]/; 
   for (i = 0; i < str.length; i++) {
-    if (str[i] == " ") {
-      outputStr += str[i];
-    }
-    else {
+    // test whether capital letter
+    if (regex.test(str[i])) {
       var letterASCII = str.charCodeAt(i);
       var newLetterASCII = letterASCII % 26 + 65; 
       var newLetter = String.fromCharCode(newLetterASCII);
       outputStr += newLetter;
     }
+    else {
+      outputStr += str[i];
+    }
     
   }
   return outputStr;
 }
+
+rot13("SERR PBQR PNZC");
